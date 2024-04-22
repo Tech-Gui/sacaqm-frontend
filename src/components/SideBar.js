4d4035e56eae129ee6edd5e3a52c657fadcfa353
Methembe Tshuma <53082789+CongoBeast@users.noreply.github.com>
2024-04-22 15:19:16 +0200

    nav bar updates

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { Navbar, Nav, Image, Button } from "react-bootstrap";
-import { Link, useLocation } from "react-router-dom"; // Import useLocation
+import React, { useState } from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import { Link, useLocation } from "react-router-dom";
 
 import { RxDashboard, RxPinLeft } from "react-icons/rx";
 import { GiTruck } from "react-icons/gi";
@@ -13,24 +13,27 @@ import { SiMicrosoftexcel } from "react-icons/si";
 import { RiAlarmWarningFill } from "react-icons/ri";
 
 function Sidebar() {
-  // Get the current location
   const location = useLocation();
+  const [isOpen, setIsOpen] = useState(false);
 
-  // Define a function to determine if a link is active
   const isLinkActive = (path) => location.pathname === path;
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <Navbar
       expand="lg"
       variant="light"
-      className="flex-column"
+      className="flex-column d-none d-lg-block"
       style={{
         width: "15rem",
-        // backgroundColor: "#1B2791",
         backgroundColor: "#fff",
         boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)",
+        transition: "width 0.3s ease",
       }}>
-      <Navbar.Brand as={Link} to="/dashboard">
+      <Navbar.Brand as={Link} to="/dashboard" >
         <img
           src="logo.png"
           alt="Imat Tech Logo"
@@ -38,8 +41,9 @@ function Sidebar() {
         />
       </Navbar.Brand>
 
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
+      <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={toggleSidebar} />
+
+      <Navbar.Collapse id="basic-navbar-nav" className={!isOpen && "d-none d-lg-block"}>
         <Nav className="flex-column gap-4">
           <Nav.Link
             as={Link}
@@ -48,9 +52,10 @@ function Sidebar() {
               textAlign: "left",
               color: isLinkActive("/dashboard") ? "#1B2791" : "#3DA2E6",
             }}>
-            <RxDashboard size={30} />{" "}
+            <RxDashboard size={30} />
             <span style={{ marginLeft: "1rem" }}>Dashboard</span>
-          </Nav.Link>{" "}
+          </Nav.Link>
+
           <Nav.Link
             as={Link}
             to="/stations"
@@ -60,18 +65,8 @@ function Sidebar() {
             }}>
             <SiMicrosoftexcel size={30} />
             <span style={{ marginLeft: "1rem" }}>All Stations</span>
-          </Nav.Link>{" "}
-          {/* <Nav.Link
-            as={Link}
-            to="/reports"
-            style={{
-              textAlign: "left",
-              color: isLinkActive("/reports") ? "#EEE" : "#3DA2E6",
-            }}>
-            {/* <IoPieChartSharp size={30} /> */}
-          {/* <RiAlarmWarningFill size={30} />
-            <span style={{ marginLeft: "1rem" }}>Hotspots</span>
-          </Nav.Link> */}
+          </Nav.Link>
+
           <Nav.Link
             as={Link}
             to="/analytics"
@@ -80,8 +75,8 @@ function Sidebar() {
               color: isLinkActive("/analytics") ? "#1B2791" : "#3DA2E6",
             }}>
             <IoPieChartSharp size={30} />
-            <span style={{ marginLeft: "1rem" }}>Analytics </span>
-          </Nav.Link>{" "}
+            <span style={{ marginLeft: "1rem" }}>Analytics</span>
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
 
---
+++ src/components/topNavBar.js
@@ -0,0 +1,69 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link, useLocation } from "react-router-dom";
+import React, { useState } from "react";
+
+import { RxDashboard, RxPinLeft } from "react-icons/rx";
+import { GiTruck } from "react-icons/gi";
+import { IoPieChartSharp } from "react-icons/io5";
+import { FaFileInvoiceDollar } from "react-icons/fa";
+import { IoIosPeople } from "react-icons/io";
+import { GiCoalWagon } from "react-icons/gi";
+import { BiSupport } from "react-icons/bi";
+import { SiMicrosoftexcel } from "react-icons/si";
+import { RiAlarmWarningFill } from "react-icons/ri";
+
+
+function TopNavBar() {
+
+  return (
+    <Navbar collapseOnSelect expand="md" className="bg-body-tertiary d-lg-none mb-3 mx p-1">
+      <Container>
+        {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
+        <Navbar.Brand as={Link} to="/dashboard" >
+        <img
+          src="logo.png"
+          alt="Imat Tech Logo"
+          style={{ maxHeight: "50px", maxWidth: "auto" }}
+        />
+      </Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav ">
+    
+          <Nav className='ml-auto'>
+
+          <Nav.Link
+            as={Link}
+            to="/dashboard"
+            >
+            <RxDashboard size={20} />
+            <span style={{ marginLeft: "1rem" }}>Dashboard</span>
+          </Nav.Link>
+
+            <Nav.Link
+            as={Link}
+            to="/stations"
+            >
+            <SiMicrosoftexcel size={20} />
+            <span style={{ marginLeft: "1rem" }}>All Stations</span>
+          </Nav.Link>
+
+          <Nav.Link
+            as={Link}
+            to="/analytics">
+            <IoPieChartSharp size={20} />
+            <span style={{ marginLeft: "0.5rem" }}>Analytics</span>
+          </Nav.Link>
+
+
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default TopNavBar;
