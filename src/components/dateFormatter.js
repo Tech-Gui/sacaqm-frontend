44074b90c930c417a88857b19ec5e8c9453ea741
Isaiah Tadiwanashe Chiraira <itchiraira@gmail.com>
2024-05-30 12:39:06 +0200

    Fixed time format

--- src/components/dateFormatter.js
+++ src/components/dateFormatter.js
@@ -1,4 +1,10 @@
-import { format, isToday, isYesterday, parseISO } from 'date-fns';
+import {
+  format,
+  isToday,
+  isYesterday,
+  parseISO,
+  differenceInCalendarDays,
+} from "date-fns";
 
 export const formatLastSeen = (timestamp) => {
   const date = parseISO(timestamp);
@@ -7,7 +13,9 @@ export const formatLastSeen = (timestamp) => {
     return format(date, "h:mm a");
   } else if (isYesterday(date)) {
     return `Yesterday ${format(date, "h:mm a")}`;
-  } else {
+  } else if (differenceInCalendarDays(new Date(), date) <= 6) {
     return format(date, "EEEE h:mm a"); // e.g., Tuesday 12:55 pm
+  } else {
+    return format(date, "dd/MM/yyyy"); // e.g., 28/05/2024
   }
 };
