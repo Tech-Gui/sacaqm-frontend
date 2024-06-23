bfd35a104b5688bb68b1be0e20fd642bdee7afad
Isaiah Chiraira <72510155+Tech-Gui@users.noreply.github.com>
2024-06-23 14:07:01 +0200

    Update StationsTable.js

--- src/components/StationsTable.js
+++ src/components/StationsTable.js
@@ -1,6 +1,6 @@
-
 import React from "react";
 import { Table } from "react-bootstrap";
+import { formatLastSeen } from "./dateFormatter";
 
 function StationsTable({ data }) {
   const tableHeight = "80vh"; // Set the desired table height
@@ -9,7 +9,7 @@ function StationsTable({ data }) {
     <div style={{ height: tableHeight, overflowY: "scroll" }} className="mt-2">
       <Table striped hover>
         <thead>
-          <tr> 
+          <tr>
             <th style={headerStyle}>STATION</th>
             <th style={headerStyle}>PROVINCE</th>
             <th style={headerStyle}>CITY</th>
@@ -18,37 +18,28 @@ function StationsTable({ data }) {
             <th style={headerStyle}>LATITUDE</th>
             <th style={headerStyle}>SENSOR IDs</th>
             <th style={headerStyle}>LAST SEEN</th>
-            {/* <th style={headerStyle}>ACTIVE</th> */}
-            {/* <th style={headerStyle}>ID</th> */}
-            {/* <th style={headerStyle}>PM1.0</th>
-            <th style={headerStyle}>PM2.5</th>
-            <th style={headerStyle}>PM4.0</th>
-            <th style={headerStyle}>PM10.0</th>
-            <th style={headerStyle}>NOX</th>
-            <th style={headerStyle}>VOC</th> */}
+        
           </tr>
         </thead>
         <tbody>
           {data.map((station, index) => (
             <tr key={index}>
-              <td style={{ textAlign: 'left' }}>{station.name}</td>
-              <td style={{ textAlign: 'left' }}>{station.province}</td>
-              <td style={{ textAlign: 'left' }}>{station.city}</td>
-              <td style={{ textAlign: 'left' }}>{station.description}</td>
-              <td style={{ textAlign: 'left' }}>{station.longitude}</td>
-              <td style={{ textAlign: 'left' }}>{station.latitude}</td>
-              <td style={{ textAlign: 'left' }}>{station.sensorIds.join(', ')}</td>
-              <td style={{ textAlign: 'left' }}>{station.lastSeen}</td>
+              <td style={{ textAlign: "left" }}>{station.name}</td>
+              <td style={{ textAlign: "left" }}>{station.province}</td>
+              <td style={{ textAlign: "left" }}>{station.city}</td>
+              <td style={{ textAlign: "left" }}>{station.description}</td>
+              <td style={{ textAlign: "left" }}>{station.longitude}</td>
+              <td style={{ textAlign: "left" }}>{station.latitude}</td>
+              <td style={{ textAlign: "left" }}>
+                {station.sensorIds.join(", ")}
+              </td>
+              <td style={{ textAlign: "left" }}>
+                {station.lastSeen
+                  ? formatLastSeen(station.lastSeen)
+                  : "No data"}
+              </td>
 
-
-              {/* <td>{item.ACTIVE}</td> */}
-              {/* <td>{item.SENSOR_ID}</td> */}
-              {/* <td>{item.PM1_0}</td>
-              <td>{item.PM2_5}</td>
-              <td>{item.PM4_0}</td>
-              <td>{item.PM10_0}</td>
-              <td>{item.NOX}</td>
-              <td>{item.VOC}</td> */}
+           
             </tr>
           ))}
         </tbody>
@@ -62,6 +53,6 @@ const headerStyle = {
   fontWeight: "bold",
   fontSize: "10px",
   fontFamily: "Helvetica Neue",
-  textAlign: "left"
+  textAlign: "left",
 };
 export default StationsTable;
