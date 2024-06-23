7146d2bd19d86a4ecdc56265f8bce745221c4a4a
Isaiah Chiraira <72510155+Tech-Gui@users.noreply.github.com>
2024-06-23 13:45:52 +0200

    Update StationContext.js

--- src/contextProviders/StationContext.js
+++ src/contextProviders/StationContext.js
@@ -1,6 +1,6 @@
-import React, { createContext, useState, useEffect } from 'react';
-import axios from 'axios';
-import { formatLastSeen } from '../components/dateFormatter'; // Adjust the path as needed
+import React, { createContext, useState, useEffect } from "react";
+import axios from "axios";
+import { formatLastSeen } from "../components/dateFormatter"; // Adjust the path as needed
 
 export const StationContext = createContext();
 
@@ -9,33 +9,15 @@ export const StationProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const fetchLastSeen = async (stationId) => {
-    try {
-      const response = await axios.get(`https://try-again-test-isaiah.app.cern.ch/api/stations/${stationId}/sensorData`);
-      const sensorData = response.data.reverse();
-      if (sensorData.length > 0) {
-        return formatLastSeen(sensorData[sensorData.length - 1].timestamp);
-      }
-      return 'No data';
-    } catch (err) {
-      console.error(`Error fetching sensor data for station ${stationId}:`, err);
-      return 'Error';
-    }
-  };
-
   const fetchStations = async () => {
     try {
       setLoading(true);
-      const response = await axios.get('https://try-again-test-isaiah.app.cern.ch/api/stations');
+      const response = await axios.get(
+        "https://try-again-test-isaiah.app.cern.ch/api/stations"
+      );
       const stationsData = response.data;
 
-      // Fetch last seen timestamp for each station
-      const stationsWithLastSeen = await Promise.all(stationsData.map(async (station) => {
-        const lastSeen = await fetchLastSeen(station._id);
-        return { ...station, lastSeen };
-      }));
-
-      setStations(stationsWithLastSeen);
+      setStations(stationsData);
     } catch (err) {
       setError(err);
     } finally {
@@ -48,7 +30,8 @@ export const StationProvider = ({ children }) => {
   }, []);
 
   return (
-    <StationContext.Provider value={{ stations, loading, error, fetchStations }}>
+    <StationContext.Provider
+      value={{ stations, loading, error, fetchStations }}>
       {children}
     </StationContext.Provider>
   );
