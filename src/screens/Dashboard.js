af8ceb91eb7d075c905ea11c1fe5526866aa8d46
Isaiah Chiraira <72510155+Tech-Gui@users.noreply.github.com>
2024-07-05 08:29:13 +0200

    Update Dashboard.js

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -451,7 +451,7 @@ function Dashboard() {
                 border: "none",
                 boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)",
               }}>
-              {getStationNameByStationId(selectedSensor) || "Thulani"}
+              {getStationNameByStationId(selectedSensor) || "Origin Center -1"}
             </Dropdown.Toggle>
             <Dropdown.Menu style={{ maxHeight: "80vh", overflowY: "scroll" }}>
               {stations.map((station, index) => (
