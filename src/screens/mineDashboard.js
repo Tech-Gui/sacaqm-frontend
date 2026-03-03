20300a1822909fa842725e1be97a054d2c80c034
mkarmoud <manal.karmoude@cern.ch>
2026-03-03 16:57:25 +0000

    fix the private dashboard and add dba update

--- src/screens/mineDashboard.js
+++ src/screens/mineDashboard.js
@@ -45,9 +45,8 @@ function MineDashboard() {
 
   const { nodeData, setNodeData, fetchNodeData } = useContext(DataContext);
   const { selectedType, handleTypeSelect } = useDataType();
-  const [stations, setStations] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { stations, setStations, loading, setLoading, error, setError,fetchStations } =
+      useContext(StationContext)
   const [sensorNameMap, setSensorNameMap] = useState({});
 
   const [startDate, setStartDate] = useState("");
